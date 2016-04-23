aa337d71f8c20d7a823b0703e261ac8be8b869d7
GameMaster9000 <kirillsakharov04@gmail.com>
2016-04-22 17:40:52 -0700

    Update hello_app.py

--- hello_app.py
+++ hello_app.py
@@ -4,7 +4,16 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+import random,urllib2
+
+activities_to_do = urllib2.urlopen('https://raw.githubusercontent.com/bellcodo/potential-octo-chainsaw/master/code_activities_we_like.lst').read()                  
+activities_to_do = activities_to_do.split()
+going_to_do = random.choice(activities_to_do)
+
+
+print "We can do the following" + str(activities_to_do)
+print "We are going to do " + going_to_do 
+return typing_choice
 
 if __name__ == "__main__":
     # go get the PORT from the environment
